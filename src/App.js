e0b2d8a080042aa9bb2ff7c95d6b4393be2b32f8
Polly Yospan <porgroovy@gmail.com>
2022-05-17 11:47:20 -0400

    create user information form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h2>
+        How many calories in a healthy rate should I eat per day to lose weight
+        within 12 weeks?
+      </h2>
+      <form>
+        <label htmlFor="gender">Gender</label>
+        <input name="gender" type="radio" id="inputMale" value="male" />
+        <label for="male">Male</label>
+        <input name="gender" type="radio" id="inputFemale" value="female" />
+        <label for="female">Female</label>
+        <label htmlFor="age">Age</label>
+        <input name="age" type="text" id="inputAge" placeholder="Age" />
+        <label htmlFor="height">Height</label>
+        <input name="feet" type="number" id="inputFeet" placeholder="feet" />
+        <input
+          name="inches"
+          type="number"
+          id="inputInches"
+          placeholder="inches"
+        />
+        <label htmlFor="weight">Weight</label>
+        <input
+          name="weight"
+          type="number"
+          id="inputWeight"
+          placeholder="pounds"
+        />
+        <label htmlFor="goalWeight">Goal Weight</label>
+        <input
+          name="goalWeight"
+          type="number"
+          id="inputGoalWeight"
+          placeholder="pounds"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default App;
